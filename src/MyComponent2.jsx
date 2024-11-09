6d4af763e4f5557002ef76014c8834f4d094f216
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2024-11-09 04:08:56 -0800

    first commit 2

--- src/MyComponent2.jsx
+++ src/MyComponent2.jsx
@@ -4,11 +4,25 @@ import { useState } from 'react'
 import React from 'react'
 
 const MyComponent2 = () => {
+    const [name,setName]=useState('')//for string
+    const [quantity, setQuantity] = useState()
+
+    function displayName(e){
+        setName(e.target.value)
+    }
+    function quantityChange(e){
+        setQuantity(e.target.value)
+    }
+
 
 
   return (
     <div>
+        <input type="text" onChange={displayName} value={name} />
+        <p>Name: {name}</p>
 
+        <input type='number' value={quantity} onChange={quantityChange} />
+        <p>qunatity change: {quantity}</p>
     </div>
   )
 }
